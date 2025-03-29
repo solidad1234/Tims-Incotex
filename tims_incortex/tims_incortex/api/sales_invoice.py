14fc4fa05063c1fc2d5c95aaa78bf1144ad68539
maniamartial <martialmania19@gmail.com>
2025-03-29 09:00:08 +0300

    fix - Minor formats

--- tims_incortex/tims_incortex/api/sales_invoice.py
+++ tims_incortex/tims_incortex/api/sales_invoice.py
@@ -59,26 +59,24 @@ class TimsInvoice:
         hs_code = tax_amount(self.invoice)
         """Prepare invoice data for TIMS API."""
         return {
-            "invoice_date": self.invoice.posting_date.strftime("%d_%m_%Y"),
-            "invoice_number": self.invoice.name,
+            "invoice_date": self.invoice.posting_date.strftime("%d-%m-%Y"),
+            "invoice_number": format_invoice_number(self.invoice.name),
             "invoice_pin": self.settings["company_pin"],
             "customer_pin": self.invoice.tax_id or "",
             "customer_exid": "",
-            "grand_total": str(self.invoice.grand_total),
-            "net_subtotal": str(self.invoice.net_total),
-            "tax_total": str(self.invoice.total_taxes_and_charges),
-            "net_discount_total": str(self.invoice.discount_amount or "0.00"),
-            "sel_currency": self.invoice.currency,
+            "grand_total": abs(self.invoice.grand_total),
+            "net_subtotal": abs(self.invoice.net_total),
+            "tax_total": abs(self.invoice.total_taxes_and_charges),
+            "net_discount_total": abs(self.invoice.discount_amount or 0.00),
+            "sel_currency": currency_code(self.invoice.currency),
             "rel_doc_number": rel_doc_number,
             "items_list": [
                 f"{hs_code if self.invoice.total_taxes_and_charges == 0 else ''} "
-                f"{re.sub(r'[^a-zA-Z0-9]', '', i.item_code)} {i.qty:.2f} {i.rate:.2f} {i.amount:.2f}"
+                f"{re.sub(r'[^a-zA-Z0-9]', '', i.item_code)} {abs(i.qty):.2f} {abs(i.rate):.2f} {abs(i.amount):.2f}"
                 for i in self.invoice.items
             ]
         }
 
-
-    
     def _update_invoice(self, response_data):
         """Update invoice with TIMS API response using set_value."""
         frappe.db.set_value("Sales Invoice", self.invoice.name, {
@@ -238,10 +236,18 @@ def is_active(company):
     return settings.get("active")
 
 def tax_amount(invoice):
+    hs_code = ''
     if not invoice.total_taxes_and_charges:
         customer = invoice.customer
         tax_category = frappe.get_value("Customer", customer, "tax_category")
         hs_code = frappe.get_value("Tax Category", tax_category, "custom_hs_code")
-        return hs_code
+    return hs_code if hs_code else ""
     
     
+def currency_code(currency):
+    if currency == "KES":
+        return "Ksh"
+    
+def format_invoice_number(invoice_number):
+    """Format the invoice number to ensure there is no special charaters"""
+    return re.sub(r"[^a-zA-Z0-9]", "", invoice_number)
