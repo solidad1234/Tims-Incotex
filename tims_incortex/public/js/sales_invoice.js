3497c4b0fbd3691cb11ec26f531d2cae13582e22
maniamartial <martialmania19@gmail.com>
2026-01-13 23:07:30 +0300

    feat: update the button display

--- tims_incortex/public/js/sales_invoice.js
+++ tims_incortex/public/js/sales_invoice.js
@@ -1,7 +1,12 @@
 frappe.ui.form.on("Sales Invoice", {
     refresh: function (frm) {
+        const status = (frm.doc.custom_signing_status || "").trim();
         // Show "Submit e-Invoice" button when invoice is pending or failed
-        if (frm.doc.docstatus === 1 && ["Failed", "Pending",""].includes(frm.doc.custom_signing_status)) {
+        if (
+    frm.doc.docstatus === 1 &&
+    ["Failed", "Pending", "", null].includes(status)
+) {
+
             frm.add_custom_button("Submit e-Invoice", function () {
                 frappe.call({
                     method: "tims_incortex.tims_incortex.api.sales_invoice.sign_single_invoice",
